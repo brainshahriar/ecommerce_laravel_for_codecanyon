a84a05a3f9de41df8927f204cd8a30a1af369b4f
brain <shahriarmehedi95@gmail.com>
2022-01-11 21:34:04 +0600

    category

---
+++ resources/views/admin/content/left-sidebar.blade.php
@@ -0,0 +1,22 @@
+<aside class="left-sidebar">
+    <ul id="slide-out" class="sidenav">
+        <li>
+            <ul class="collapsible">
+                <li>
+                    <a href="" class="collapsible-header"><i class="material-icons">dashboard</i><span class="hide-menu"> Dashboard</span></a>
+                </li>
+                 <li>
+                    <a href="javascript: void(0);" class="collapsible-header has-arrow"><i class="material-icons">equalizer</i><span class="hide-menu"> Products </span></a>
+                    <div class="collapsible-body">
+                        <ul>
+                            <li><a href="sidebar-type-minisidebar.html"><i class="material-icons">photo_size_select_small</i><span class="hide-menu">Brand</span></a></li>
+                            <li><a href="sidebar-type-iconbar.html"><i class="material-icons">picture_in_picture</i><span class="hide-menu">Category</span></a></li>
+                            <li><a href="sidebar-type-overlay.html"><i class="material-icons">low_priority</i><span class="hide-menu">Sub Category</span></a></li>
+                            <li><a href="sidebar-type-fullsidebar.html"><i class="material-icons">present_to_all</i><span class="hide-menu">Sub SubCategory</span></a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
